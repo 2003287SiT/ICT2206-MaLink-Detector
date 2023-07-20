623b08aadcefa18fced567c6faa43615bdd28b26
Derrickeee <2102080@sit.singaporetech.edu.sg>
2023-07-20 17:56:12 +0800

    insert requirements.txt file, and make changes to conform to PEP 8

--- popup.js
+++ popup.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var detectLinksButton = document.getElementById('detectLinksBtn');
     var generateHTMLButton = document.getElementById('generateHTMLBtn'); // New button element
     var resultElement = document.getElementById('result');
-  
+
+    var uniqueLinks = new Set(); // Use a Set to store unique links
+
     // Add click event listener for Detect Links button function
     detectLinksButton.addEventListener('click', function() {
       // Query the active tab in the current window
@@ -18,10 +20,11 @@ document.addEventListener('DOMContentLoaded', function() {
           }
           // If there are links in the returned results
           if (results && results.length > 0) {
-            // Get the array of links from the results
-            var links = results[0];
-            // Set the innerText of the resultElement to display the links
-            resultElement.innerText = links.join('\n');
+                    var links = results[0];
+                    // Add links to the uniqueLinks Set to eliminate duplicates
+                    links.forEach(link => uniqueLinks.add(link));
+                    // Update the resultElement to display the unique links
+                    resultElement.innerText = Array.from(uniqueLinks).join('\n');
           }
         });
       });
@@ -40,23 +43,78 @@ document.addEventListener('DOMContentLoaded', function() {
   
     // Function to generate the HTML page with the links
     function generateHTML(links) {
-      var htmlContent = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-          <title>Links Found by MaLink Detector </title>
-        </head>
-        <body>
-          <h1>Links Found</h1>
-          <ul>
-            ${links.map(link => `<li><a href="${link}">${link}</a></li>`).join('\n')}
-          </ul>
-        </body>
-        </html>
-      `;
-  
-      // Open the generated HTML page in a new tab
-      chrome.tabs.create({ url: 'data:text/html;charset=utf-8,' + encodeURIComponent(htmlContent) });
+        var linksPerPage = 50; // Set the number of links to display per page
+        var numPages = Math.ceil(links.length / linksPerPage);
+
+        var htmlPages = [];
+        for (let pageNum = 0; pageNum < numPages; pageNum++) {
+            var startIdx = pageNum * linksPerPage;
+            var endIdx = startIdx + linksPerPage;
+            var pageLinks = links.slice(startIdx, endIdx);
+
+            var pageContent = `
+                <div class="page page-${pageNum}">
+                    <h1>Links Found - Page ${pageNum + 1}</h1>
+                    <ul>
+                        ${pageLinks.map(link => `<li><a href="${link}">${link}</a></li>`).join('\n')}
+                    </ul>
+                </div>
+            `;
+
+            htmlPages.push(pageContent);
+        }
+
+        var fullHtmlContent = `
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <title>Links Found by MaLink Detector</title>
+            </head>
+            <body>
+                ${htmlPages.join('\n')}
+                <div id="pagination">
+                    <button id="prevBtn" disabled>Previous</button>
+                    <button id="nextBtn">Next</button>
+                </div>
+            </body>
+            <script>
+                var currentPage = 0;
+                var numPages = ${numPages};
+
+                function showPage(pageNum) {
+                    var pages = document.querySelectorAll('.page');
+                    for (let i = 0; i < pages.length; i++) {
+                        pages[i].style.display = i === pageNum ? 'block' : 'none';
+                    }
+
+                    var prevBtn = document.getElementById('prevBtn');
+                    var nextBtn = document.getElementById('nextBtn');
+
+                    prevBtn.disabled = pageNum === 0;
+                    nextBtn.disabled = pageNum === numPages - 1;
+                }
+
+                document.getElementById('prevBtn').addEventListener('click', function() {
+                    if (currentPage > 0) {
+                        currentPage--;
+                        showPage(currentPage);
+                    }
+                });
+
+                document.getElementById('nextBtn').addEventListener('click', function() {
+                    if (currentPage < numPages - 1) {
+                        currentPage++;
+                        showPage(currentPage);
+                    }
+                });
+
+                showPage(currentPage);
+            </script>
+            </html>
+        `;
+
+        // Open the generated HTML page in a new tab
+        chrome.tabs.create({ url: 'data:text/html;charset=utf-8,' + encodeURIComponent(fullHtmlContent) });
     }
   });
   
