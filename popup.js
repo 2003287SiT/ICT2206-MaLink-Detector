618fe6a407ddab1667f096f7e9d40f27aeee94e8
2003287SiT <139607634+2003287SiT@users.noreply.github.com>
2023-07-21 16:42:58 +0800

    UPDATE: Manifest v3 extension
    Base Extension code for Manifest v3

---
+++ popup.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("detectLinksBtn").addEventListener("click", () => {
+      console.log("Detect Links button clicked!");
+      // Add your link detection logic here.
+    });
+  
+    document.getElementById("generateHtmlReportBtn").addEventListener("click", () => {
+      console.log("Generate HTML Report button clicked!");
+      // Add your HTML report generation logic here.
+    });
+  });
+  
