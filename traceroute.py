9329241998995b09c286795ed844b0ccbfa0caa2
Derrickeee <2102080@sit.singaporetech.edu.sg>
2023-07-22 22:16:46 +0800

    modify traceroute.py by formatting

--- traceroute.py
+++ traceroute.py
@@ -19,9 +19,10 @@ def traceroute(target, max_hops=30):
     target = target.replace("http://", "").replace("www.", "")
 
     print(f"Traceroute to {Fore.YELLOW}{target}{Fore.RESET} ({socket.gethostbyname(target)})")
-
+    print("-" * 50)
     tracer_list = []
-
+    print(colorama.Fore.GREEN + "{:<5} {:<20} {:<30} {:<10}".format("TTL", "IP Address", "Hostname",
+                                                                    "RTT (ms)" + colorama.Style.RESET_ALL))
     for ttl in range(1, max_hops + 1):
         packet = IP(dst=target, ttl=ttl) / ICMP()
 
@@ -30,15 +31,14 @@ def traceroute(target, max_hops=30):
 
         if reply is None:
             # No response received, print a timeout message and continue to the next hop
-            print(f"{ttl} * * *")
+            print("{:<5} {:<20} {:<30} {:10.4s}".format(ttl, "*", "*", "*"))
             tracer_list.append({"TTL": ttl, "IP Address": "*", "Hostname": "*", "RTT": "*"})
         else:
             # A response is received
             ip_address = reply.src
             hostname = resolve(ip_address)
             rtt = round(reply.time * 1000, 2)  # Convert to milliseconds
-
-            print(f"{ttl} {ip_address} {hostname} {rtt} ms")
+            print("{:<5} {:<20} {:<30} {:<10.4f}".format(ttl, ip_address, hostname, rtt))
             tracer_list.append({"TTL": ttl, "IP Address": ip_address, "Hostname": hostname, "RTT": rtt})
 
         # Check if the response is an ICMP Echo Reply (type 0)
