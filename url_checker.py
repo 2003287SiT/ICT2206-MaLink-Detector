c65f3b423399a253c30cc85746e7c8ca72d1ad9b
Derrickeee <2102080@sit.singaporetech.edu.sg>
2023-07-23 09:20:09 +0800

    check for suspicious URLs

--- url_checker.py
+++ url_checker.py
@@ -1,15 +1,23 @@
-def is_suspicious_url(url):
-    suspicious_keywords = {"login", "signin", "account", "bank", "paypal", "secure",
-                           "confirm", "password", "verify", "update", "billing",
-                           "malware", "trojan", "virus", "spyware", "keylogger", "backdoor", "exploit",
-                           "buy", "cheap", "discount", "sale", "deal", "offer"}
+import ipaddress
 
+def is_suspicious_url(url):
     def contains_suspicious_keyword(url):
+        suspicious_keywords = {"login", "signin", "account", "bank", "paypal", "secure",
+                               "confirm", "password", "verify", "update", "billing",
+                               "malware", "trojan", "virus", "spyware", "keylogger", "backdoor", "exploit",
+                               "buy", "cheap", "discount", "sale", "deal", "offer"}
         return any(keyword in url.lower() for keyword in suspicious_keywords)
 
     def is_ip_address(url):
-        parts = url.split(".")
-        return len(parts) == 4 and all(part.isdigit() and 0 <= int(part) <= 255 for part in parts)
+        # Extract the hostname from the URL
+        hostname = url.split("//")[-1].split("/")[0]
+
+        try:
+            # Attempt to create an IP address object from the hostname
+            ipaddress.ip_address(hostname)
+            return True
+        except ValueError:
+            return False
 
     def is_shortened_url(url):
         shortened_domains = {"bit.ly", "goo.gl", "t.co", "ow.ly", "tinyurl"}
@@ -19,14 +27,29 @@ def is_suspicious_url(url):
         suspicious_extensions = {".exe", ".dll", ".bat", ".cmd", ".vbs", ".js", ".jar"}
         return any(url.lower().endswith(extension) for extension in suspicious_extensions)
 
-    return is_ip_address(url) or contains_suspicious_keyword(url) or is_shortened_url(url) or has_suspicious_extension(url)
+    def has_suspicious_query_param(url):
+        suspicious_query_params = {"cmd", "exec", "eval", "javascript", "script"}
+        query_params = url.split("?")[-1].split("&")
+        return any(param.split("=")[0].lower() in suspicious_query_params for param in query_params)
+
+    def has_suspicious_tags(url):
+        suspicious_tags = {"script", "iframe", "embed"}
+        return any(tag in url.lower() for tag in suspicious_tags)
+
+    return (
+        is_ip_address(url) or
+        contains_suspicious_keyword(url) or
+        is_shortened_url(url) or
+        has_suspicious_extension(url) or
+        has_suspicious_query_param(url) or
+        has_suspicious_tags(url)
+    )
 
 # Test the function with some example URLs
 if __name__ == "__main__":
     urls = [
         "https://www.example.com/some-page",
         "http://192.168.0.1",
-        "https://www.suspicious-site.com/malicious.php",
         "https://www.valid-site.com/",
         "https://www.phishing-site.com/login.php",
         "https://www.update-site.com/1.0.0/update",
@@ -39,3 +62,4 @@ if __name__ == "__main__":
 
     for url in urls:
         print(f"{url}: {is_suspicious_url(url)}")
+
