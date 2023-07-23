f06f2aab1c4af0df320b8347a918589be3f693e3
Derrickeee <2102080@sit.singaporetech.edu.sg>
2023-07-23 09:01:54 +0800

    check for suspicious URLs

--- url_checker.py
+++ url_checker.py
@@ -1,45 +1,41 @@
-import re
-
 def is_suspicious_url(url):
-    # Define a list of regular expressions to match against different suspicious patterns
-    suspicious_patterns = [
-        r"\b(?:https?://)?\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b",  # IP address in the URL
-        r"\b(?:https?://)?(?:www\.)?(?:[a-zA-Z0-9-]+)\.(?:[a-zA-Z]{2,})/.*\.php\b",  # PHP file in the URL
-        r"\b(?:https?://)?(?:www\.)?(?:[a-zA-Z0-9-]+)\.(?:[a-zA-Z]{2,})/.*\b(?:login|signin|account|bank|paypal|secure|confirm|password|verify|update|billing)\b",  # Phishing-related patterns
-        r"\b(?:https?://)?(?:www\.)?(?:[a-zA-Z0-9-]+)\.(?:[a-zA-Z]{2,})/.*\b(?:update|install|download|patch)\b(?:\d+\.\d+)+",  # Fake software update URLs
-        r"\b(?:https?://)?(?:www\.)?(?:[a-zA-Z0-9-]+)\.(?:[a-zA-Z]{2,})/.*\b(?:malware|trojan|virus|spyware|keylogger|backdoor|exploit)\b",  # Malware distribution URLs
-        r"\b(?:https?://)?(?:www\.)?(?:[a-zA-Z0-9-]+)\.(?:[a-zA-Z]{2,})/.*\.(?:exe|dll|bat|cmd|vbs|js|jar)\b",  # Suspicious file extensions
-        r"\b(?:https?://)?(?:www\.)?(?:[a-zA-Z0-9-]+)\.(?:[a-zA-Z]{2,})/\?.*\b(?:cmd|exec|eval|javascript|script)\b",  # Suspicious query parameters
-        r"\b(?:https?://)?(?:www\.)?(?:[a-zA-Z0-9-]+)\.(?:[a-zA-Z]{2,})/.*\b(?:bit\.ly|goo\.gl|t\.co|ow\.ly|tinyurl)\b",  # URL shorteners
-        r"\b(?:https?://)?(?:www\.)?(?:[a-zA-Z0-9-]+)\.(?:[a-zA-Z]{2,})/\?(?:.*&)?(?:u|url|link|dest|target|redir|redirect)=\b",  # URL redirection in query parameters
-        r"\b(?:https?://)?(?:www\.)?(?:[a-zA-Z0-9-]+)\.(?:[a-zA-Z]{2,})/.*\b(?:script|iframe|embed)\b",  # Script, iframe, or embed tags in the URL
-        r"\b(?:https?://)?(?:www\.)?(?:[a-zA-Z0-9-]+)\.(?:[a-zA-Z]{2,})/.*\b(?:buy|cheap|discount|sale|deal|offer)\b",  # URLs with keywords suggesting suspicious offers
-    ]
+    suspicious_keywords = {"login", "signin", "account", "bank", "paypal", "secure",
+                           "confirm", "password", "verify", "update", "billing",
+                           "malware", "trojan", "virus", "spyware", "keylogger", "backdoor", "exploit",
+                           "buy", "cheap", "discount", "sale", "deal", "offer"}
 
-    # Check if the URL matches any of the suspicious patterns
-    for pattern in suspicious_patterns:
-        if re.search(pattern, url):
-            return True
+    def contains_suspicious_keyword(url):
+        return any(keyword in url.lower() for keyword in suspicious_keywords)
 
-    return False
+    def is_ip_address(url):
+        parts = url.split(".")
+        return len(parts) == 4 and all(part.isdigit() and 0 <= int(part) <= 255 for part in parts)
+
+    def is_shortened_url(url):
+        shortened_domains = {"bit.ly", "goo.gl", "t.co", "ow.ly", "tinyurl"}
+        return any(domain in url for domain in shortened_domains)
+
+    def has_suspicious_extension(url):
+        suspicious_extensions = {".exe", ".dll", ".bat", ".cmd", ".vbs", ".js", ".jar"}
+        return any(url.lower().endswith(extension) for extension in suspicious_extensions)
+
+    return is_ip_address(url) or contains_suspicious_keyword(url) or is_shortened_url(url) or has_suspicious_extension(url)
 
 # Test the function with some example URLs
-url1 = "https://www.example.com/some-page"
-url2 = "http://192.168.0.1"
-url3 = "https://www.suspicious-site.com/malicious.php"
-url4 = "https://www.valid-site.com/"
-url5 = "https://www.phishing-site.com/login.php"
-url6 = "https://www.update-site.com/1.0.0/update"
-url7 = "https://www.malware-site.com/malware.exe"
-url8 = "https://www.site-with-query-param.com/?cmd=execute"
-url9 = "https://www.redirect-site.com/?u=https://malicious-site.com"
+if __name__ == "__main__":
+    urls = [
+        "https://www.example.com/some-page",
+        "http://192.168.0.1",
+        "https://www.suspicious-site.com/malicious.php",
+        "https://www.valid-site.com/",
+        "https://www.phishing-site.com/login.php",
+        "https://www.update-site.com/1.0.0/update",
+        "https://www.malware-site.com/malware.exe",
+        "https://www.site-with-query-param.com/?cmd=execute",
+        "https://bit.ly/xyz",
+        "https://www.redirect-site.com/?u=https://malicious-site.com",
+        "https://www.embed-site.com/script_embed",
+    ]
 
-print(is_suspicious_url(url1))  # Output: False
-print(is_suspicious_url(url2))  # Output: True
-print(is_suspicious_url(url3))  # Output: True
-print(is_suspicious_url(url4))  # Output: False
-print(is_suspicious_url(url5))  # Output: True
-print(is_suspicious_url(url6))  # Output: True
-print(is_suspicious_url(url7))  # Output: True
-print(is_suspicious_url(url8))  # Output: True
-print(is_suspicious_url(url9)) # Output: True
+    for url in urls:
+        print(f"{url}: {is_suspicious_url(url)}")
