e3998e9628883df581d57892d786da7c690ea93d
2003287SiT <139607634+2003287SiT@users.noreply.github.com>
2023-07-26 13:35:28 +0800

    Update: Added cliformat.py
    Added cliformat.py to tidy up formatting on URL_Scooper.py.
    
    Added "Now Scooping: [domain]" info on screen to remind user which domain they are investigating

--- URL_Scooper.py
+++ URL_Scooper.py
@@ -6,6 +6,7 @@ from header import get_url_headers, print_headers
 from safebrowsing import check_url_safety, print_url_safety
 from htmlreport import generate_html
 from domainvalidation import is_multi_line_input
+from cliformat import optionheader, optionfooter, subheader
 
 art = """
 ██╗   ██╗██████╗ ██╗         ███████╗ ██████╗ ██████╗  ██████╗ ██████╗ ███████╗██████╗ 
@@ -53,43 +54,53 @@ art6 = """
       """
 
 art7 = """
+░█▄█░█░░░░░█▀▀░█░█░█▀▀░█▀▀░█░█░█▀▀░█▀▄
+░█░█░█░░░░░█░░░█▀█░█▀▀░█░░░█▀▄░█▀▀░█▀▄
+░▀░▀░▀▀▀░░░▀▀▀░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀
+      """
+
+art8 = """
 ░█▀█░█▀▄░▀█▀░█▀█░▀█▀░░░█▀█░█░░░█░░░░░█▀█░█▀█░▀█▀░▀█▀░█▀█░█▀█░█▀▀
 ░█▀▀░█▀▄░░█░░█░█░░█░░░░█▀█░█░░░█░░░░░█░█░█▀▀░░█░░░█░░█░█░█░█░▀▀█
 ░▀░░░▀░▀░▀▀▀░▀░▀░░▀░░░░▀░▀░▀▀▀░▀▀▀░░░▀▀▀░▀░░░░▀░░▀▀▀░▀▀▀░▀░▀░▀▀▀
       """
 
-art8 = """
+art9 = """
 ░█▀▀░█▀▀░█▀█░█▀▀░█▀▄░█▀█░▀█▀░▀█▀░█▀█░█▀▀░░░█░█░▀█▀░█▄█░█░░░░░█▀▄░█▀▀░█▀█░█▀█░█▀▄░▀█▀░░░░░░░░░
 ░█░█░█▀▀░█░█░█▀▀░█▀▄░█▀█░░█░░░█░░█░█░█░█░░░█▀█░░█░░█░█░█░░░░░█▀▄░█▀▀░█▀▀░█░█░█▀▄░░█░░░░░░░░░░
 ░▀▀▀░▀▀▀░▀░▀░▀▀▀░▀░▀░▀░▀░░▀░░▀▀▀░▀░▀░▀▀▀░░░▀░▀░░▀░░▀░▀░▀▀▀░░░▀░▀░▀▀▀░▀░░░▀▀▀░▀░▀░░▀░░▀░░▀░░▀░
       """
 
-art9 = """
+art0 = """
 ░█▀▀░█░█░█▀█░█▀█░█▀▀░█▀▀░░░█░█░█▀▄░█░░
 ░█░░░█▀█░█▀█░█░█░█░█░█▀▀░░░█░█░█▀▄░█░░
 ░▀▀▀░▀░▀░▀░▀░▀░▀░▀▀▀░▀▀▀░░░▀▀▀░▀░▀░▀▀▀
       """
 
-art0 = """
+artx = """
 ░█▀▀░█▀█░█▀█░█▀▄░█▀▄░█░█░█▀▀░█
 ░█░█░█░█░█░█░█░█░█▀▄░░█░░█▀▀░▀
 ░▀▀▀░▀▀▀░▀▀▀░▀▀░░▀▀░░░▀░░▀▀▀░▀
       """
 
 def display_menu():
+
     print("\n")
     print(art)
+    print("Now scooping: " + domain)
+    print("=" * 100)
     print("Choose an option:")
     print("1. WHOIS Information")
     print("2. DNSLOOKUP Information")
     print("3. SSL Information")
     print("4. Header Information")
     print("5. Location Information")
-    print("6. Safe Browsing")
-    print("7. Print All")
-    print("8. Generate Report")
-    print("9. Change URL")
-    print("0. Exit")
+    print("6. Google Safe Browsing")
+    print("7. Scooper ML Checker")
+    print("8. Print All")
+    print("9. Generate Report")
+    print("0. Change URL")
+    print("x. Exit")
 
 
 if __name__ == "__main__":
@@ -120,154 +131,103 @@ if __name__ == "__main__":
         while True:
             display_menu()
 
-            choice = input("Enter your choice (1-9): ").strip()
+            choice = input("Enter your choice (1-9, 0 or x): ").strip()
             print("\n")
 
             if choice == "1":
-                print("=" * 100)
-                print(art1)
-                print("=" * 100)
-
+                optionheader(art1)
                 whois_raw_data = get_whois_data(domain)
                 whois_info = extract_whois_info(whois_raw_data)
-
                 print(whois_info)
-
-                print("=" * 100)
-                print("=" * 100)
-                print("\n")
-                input("Press enter to continue: ")
+                optionfooter()
 
             elif choice == "2":
-                print("=" * 100)
-                print(art2)
-                print("=" * 100)
-
+                optionheader(art2)
                 dns_records = get_dns_records(domain)
                 print(get_dns_records(domain))
-
-                print("=" * 100)
-                print("=" * 100)
-                print("\n")
-                input("Press enter to continue: ")
+                optionfooter()
 
             elif choice == "3":
-                print("=" * 100)
-                print(art3)
-                print("=" * 100)
-
+                optionheader(art3)
                 supported_versions, cert_info = scan_website_ssl(domain)
-
-                print("=" * 100)
-                print("=" * 100)
-                print("\n")
-                input("Press enter to continue: ")
+                optionfooter()
 
             elif choice == "4":
-                print("=" * 100)
-                print(art4)
-                print("=" * 100)
+                optionheader(art4)
                 headers_dict = get_url_headers(domain)
                 print_headers(headers_dict)
-
-                print("=" * 100)
-                print("=" * 100)
-                print("\n")
-                input("Press enter to continue: ")
+                optionfooter()
 
             elif choice == "5":
-                print("=" * 100)
-                print(art5)
-                print("=" * 100)
+                optionheader(art5)
                 country, region, city, latitude, longitude = get_domain_location(domain)
                 print_location_info(country, region, city, latitude, longitude)
-
-                print("=" * 100)
-                print("=" * 100)
-                print("\n")
-                input("Press enter to continue: ")
+                optionfooter()
 
             elif choice == "6":
-                print("=" * 100)
-                print(art6)
-                print("=" * 100)
+                optionheader(art6)
                 check_url_safety(domain)
                 print_url_safety(domain)
-
-                print("=" * 100)
-                print("=" * 100)
-                print("\n")
-                input("Press enter to continue: ")
+                optionfooter()
 
             elif choice == "7":
-                print("=" * 100)
-                print(art7)
-                print("=" * 100)
+                optionheader(art7)
+
+                optionfooter()
+
+            elif choice == "8":
+                optionheader(art8)
                 print("Generating...")
+
+                # Option 1 Whois
+                subheader(art1)
                 whois_raw_data = get_whois_data(domain)
                 whois_info = extract_whois_info(whois_raw_data)
-                print("-" * 100)
-                print(art1)
-                print("-" * 100)
                 print(whois_info)
 
-                print("-" * 100)
-                print(art2)
-                print("-" * 100)
+                # Option 2 DNS Lookup
+                subheader(art2)
                 dns_records = get_dns_records(domain)
                 print(get_dns_records(domain))
 
-                print("-" * 100)
-                print(art3)
-                print("-" * 100)
+                # Option 3 SSL Scan
+                subheader(art3)
                 scan_website_ssl(domain)
                 print("-" * 100)
 
-                print("-" * 100)
-                print(art4)
-                print("-" * 100)
+                # Option 4 Headers
+                subheader(art4)
                 headers = get_url_headers(domain)
-                # Print the headers using the print_headers function
                 print_headers(headers)
                 print("-" * 100)
 
-                print("-" * 100)
-                print(art5)
-                print("-" * 100)
+                # Option 5 Location
+                subheader(art5)
                 country, region, city, latitude, longitude = get_domain_location(domain)
                 print_location_info(country, region, city, latitude, longitude)
-                #
-                print("-" * 100)
-                print(art6)
-                print("-" * 100)
+
+                # Option 6 Safe Browsing
+                subheader(art6)
                 print_url_safety(domain)
 
-                print("=" * 100)
-                print("=" * 100)
-                print("\n")
-                input("Press enter to continue: ")
+                # Option 7 Scooper ML
+                subheader(art7)
+                print_url_safety(domain)
 
-            elif choice == "8":
-                print("-" * 100)
-                print(art8)
-                print("-" * 100)
-                generate_html(domain)
-                print("=" * 100)
-                print("=" * 100)
-                print("\n")
-                input("Press enter to continue: ")
+                optionfooter()
 
             elif choice == "9":
-                print("-" * 100)
-                print(art9)
-                print("-" * 100)
+                optionheader(art9)
+                generate_html(domain)
+                optionfooter()
+
+            elif choice == "0":
+                optionheader(art0)
                 print("Changing URL...")
                 break
 
-            elif choice == "0":
-                print("-" * 100)
-                print(art0)
-                print("-" * 100)
+            elif choice == "x":
+                optionheader(artx)
                 print("Exiting...")
                 exit()
 
